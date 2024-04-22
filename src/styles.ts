8655989c38bbc5a73ee1bbad076e95f0725f2e80
Kauã Onorato <kauaonorato.dev@gmail.com>
2024-04-22 06:56:26 -0400

    feat: criando constante colors

--- src/styles.ts
+++ src/styles.ts
@@ -8,3 +8,14 @@ export const GlobalStyle = createGlobalStyle`
         font-family: "Raleway", sans-serif;
     }
 `
+
+export const colors = {
+    gray900: "#0C0C0D",
+    gray800: "#131313",
+    gray700: "#272727",
+    gray400: "#6F6F6F",
+    gray300: "#C8C8C8",
+    white: "#fff",
+    green: "#5FB9B0",
+    purple: "#B292FF"
+}
